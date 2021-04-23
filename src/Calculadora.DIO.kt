3c842e8fc001dc5c8b0bc89ed1c6a315630d621f
GuerraAna <ana.guerrads@hotmail.com>
2021-04-23 01:30:27 -0300

    First Commit

--- src/Calculadora.DIO.kt
+++ src/Calculadora.DIO.kt
@@ -1,4 +1,3 @@
-
 import kotlin.math.pow
 
 //Funções das operações matemáticas na calculadora. Soma-Subtração-Multiplicação-Divisão-Porcentagem-Potenciação (Sair não precisa de função).
@@ -6,11 +5,11 @@ fun soma(num1: Float, num2: Float) = num1.plus(num2)
 fun subtracao(num1: Float, num2: Float) = num1.minus(num2)
 fun multiplicacao(num1: Float, num2: Float) = num1.times(num2)
 fun divisao(num1: Float, num2: Float) = num1.div(num2)
-fun porcentagem(num1: Float, num2: Float) = num1.times(num2) / 100
+fun porcentagem(num1: Float, num2: Float) = num1.times(num2)/100
 fun potenciacao(num1: Float, num2: Float) = num1.pow(num2)
 
 //Função principal.
-fun main ( ) {
+fun main() {
     //Menu de seleção das operações e saída.
     println("Bem-vindo(a) à calculadora de Ana. Escolha a operação que deseja realizar.")
     println("1) Soma;")
@@ -39,37 +38,21 @@ fun main ( ) {
     println("2) Para não.")
 
     //Condição para a confirmação.
-    when (readLine()!!.toInt()) {
+    when(readLine()!!.toInt()) {
         1-> {
             //Condição para a operação desejada.
-            when (operacao) {
-                1-> {
-                    println("O resultado da somatória é: " + soma(num1, num2))
-                }
-                2-> {
-                    println("O resultado da subtração é: " + subtracao(num1, num2))
-                }
-                3-> {
-                    println("O resultado da multiplicação é: " + multiplicacao(num1, num2))
-                }
-                4-> {
-                    println("O resultado da divisão é: " + divisao(num1, num2))
-                }
-                5-> {
-                    println("O resultado da porcentagem é: " + porcentagem(num1, num2))
-                }
-                6-> {
-                    println("O resultado da potenciação é: " + potenciacao(num1, num2))
-                }
-                7-> {
-                    println("Que pena que você decidiu sair! Volte logo. Saindo...")
-                }
+            when(operacao) {
+                1 -> println("O resultado da somatória é: " + soma(num1, num2))
+                2 -> println("O resultado da subtração é: " + subtracao(num1, num2))
+                3 -> println("O resultado da multiplicação é: " + multiplicacao(num1, num2))
+                4 -> println("O resultado da divisão é: " + divisao(num1, num2))
+                5 -> println("O resultado da porcentagem é: " + porcentagem(num1, num2))
+                6 -> println("O resultado da potenciação é: " + potenciacao(num1, num2))
+                7 -> println("Que pena que você decidiu sair! Volte logo. Saindo...")
             }
         }
-        2-> {
-            //Condição para a operação desejada.
-            println("Tente novamente.")
-        }
+        //Condição para a operação desejada.
+        2-> println("Tente novamente.")
     }
     println("Obrigada, volte sempre!")
 }
